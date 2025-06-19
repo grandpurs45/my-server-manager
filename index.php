85033a624da4f33f8e65bfff9206eb37a5975dcd
grandpurs45 <sebastien.oudard@outlook.com>
2025-06-19 21:58:50 +0200

    Ajout de la page d'accueil index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-echo "Hello MSM!";
+$version = '0.1.0';
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+    <meta charset="UTF-8">
+    <title>My Server Manager</title>
+    </head>
+    <body>
+    <h1>Bienvenue sur My Server Manager</h1>
+    <p>Version actuelle : <?= $version ?></p>
+    <p>Date : <?= date('Y-m-d H:i:s') ?></p>
+    </body>
+</html>
